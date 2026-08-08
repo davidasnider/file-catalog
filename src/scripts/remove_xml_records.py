12654cf3712cae957bed3d5c70e234a043a822de
davidasnider <12498834+davidasnider@users.noreply.github.com>
2026-08-08 04:09:41 +0000

    docs: update PRD.md to reflect task invalidation script

--- src/scripts/remove_xml_records.py
+++ src/scripts/remove_xml_records.py
@@ -18,9 +18,11 @@ async def cleanup():
         statement = select(Document.id).where(
             (func.lower(Document.path).like("%.xml"))
             | (
-                Document.mime_type.in_(
-                    ["text/xml", "application/xml", "application/xhtml+xml"]
-                )
+                Document.mime_type.in_([
+                    "text/xml",
+                    "application/xml",
+                    "application/xhtml+xml",
+                ])
             )
         )
         result = await session.execute(statement)
